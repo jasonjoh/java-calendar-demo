013bc5b5294d00f531d8dd9cff9c45efd8554249
Jason Johnston <jasonjoh@microsoft.com>
2016-07-13 16:27:50 -0400

    Added license

--- src/main/java/com/outlook/dev/calendardemo/rawrest/CalendarOperations.java
+++ src/main/java/com/outlook/dev/calendardemo/rawrest/CalendarOperations.java
@@ -1,3 +1,4 @@
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
 package com.outlook.dev.calendardemo.rawrest;
 
 import java.io.IOException;
@@ -108,7 +109,7 @@ public class CalendarOperations extends HttpServlet {
 		if (accessToken != null){
 			// If there is a token in the session, get the expiration time
 			// Expiration is stored as Unix epoch time
-			//expiry = new Date(Long.parseLong(accessToken.getString("expires_on"))* 1000);
+			expiry = new Date(accessToken.getExpiresIn()* 1000);
 		}
 		
 		if (accessToken == null || expiry.before(new Date())) {
@@ -361,3 +362,24 @@ public class CalendarOperations extends HttpServlet {
 		return formBuilder.toString();
 	}
 }
+
+// MIT License:
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// ""Software""), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
