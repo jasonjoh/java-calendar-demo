ff2581523e545134346663d0b7113ebf3e773f5a
Jason Johnston <jasonjoh@microsoft.com>
2016-07-13 16:28:07 -0400

    Added license

--- src/main/java/com/outlook/dev/calendardemo/msgraph/GraphCalendarService.java
+++ src/main/java/com/outlook/dev/calendardemo/msgraph/GraphCalendarService.java
@@ -1,3 +1,4 @@
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
 package com.outlook.dev.calendardemo.msgraph;
 
 import com.outlook.dev.calendardemo.dto.Calendar;
@@ -128,6 +129,23 @@ public interface GraphCalendarService {
 		@Header("Authorization") String bearer
 	);
 	
+	@POST("/{version}/me/calendars/{id}/events")
+	Call<Event> createEvent(
+		@Path("version") String version,
+		@Path("id") String id,
+		@Body Event event,
+		@Header("Authorization") String bearer
+	);
+	
+	@POST("/{version}/users/{user}/calendars/{id}/events")
+	Call<Event> createEvent(
+		@Path("version") String version,
+		@Path("user") String user,
+		@Path("id") String id,
+		@Body Event event,
+		@Header("Authorization") String bearer
+	);
+	
 	@PATCH("/{version}/me/events/{id}")
 	Call<Event> updateEvent(
 		@Path("version") String version,
@@ -160,3 +178,24 @@ public interface GraphCalendarService {
 		@Header("Authorization") String bearer
 	);
 }
+
+// MIT License:
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// ""Software""), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
